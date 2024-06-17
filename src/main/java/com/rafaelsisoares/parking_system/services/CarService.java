c15e2235abf126b6b869f296d90ec635efdf005e
Rafael Soares <r.soares2015@outlook.com>
2024-06-17 18:39:38 -0300

    feat: implementando a classe carService

--- src/main/java/com/rafaelsisoares/parking_system/services/CarService.java
+++ src/main/java/com/rafaelsisoares/parking_system/services/CarService.java
@@ -1,8 +1,11 @@
 package com.rafaelsisoares.parking_system.services;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.rafaelsisoares.parking_system.entities.Car;
 import com.rafaelsisoares.parking_system.repositories.CarRepository;
+import com.rafaelsisoares.parking_system.services.exceptions.CarNotFoundException;
 
 @Service
 public class CarService {
@@ -13,4 +16,15 @@ public class CarService {
     this.carRepository = carRepository;
   }
 
+  public List<Car> getAll() {
+    return carRepository.findAll();
+  }
+
+  public Car getById(Long id) throws CarNotFoundException {
+    return carRepository.findById(id).orElseThrow(CarNotFoundException::new);
+  }
+
+  public Car createCar(Car car) {
+    return carRepository.save(car);
+  }
 }
