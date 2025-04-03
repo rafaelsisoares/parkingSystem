01cb8427e53c338e50e8d62b1022699f3198bb55
Rafael Soares <r.soares2015@outlook.com>
2025-04-03 20:02:46 -0300

    feat: criacao dos metodos de GET e POST

--- src/main/java/com/rafaelsisoares/parking_system/controllers/CarController.java
+++ src/main/java/com/rafaelsisoares/parking_system/controllers/CarController.java
@@ -2,11 +2,18 @@ package com.rafaelsisoares.parking_system.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import com.rafaelsisoares.parking_system.controllers.dto.CarCreationDto;
 import com.rafaelsisoares.parking_system.controllers.dto.CarDto;
 import com.rafaelsisoares.parking_system.services.CarService;
+import com.rafaelsisoares.parking_system.services.exceptions.CarNotFoundException;
 
 @RestController
 @RequestMapping(value = "/cars")
@@ -23,5 +30,14 @@ public class CarController {
     return carService.getAll().stream().map(CarDto::fromEntity).toList();
   }
 
+  @GetMapping("/{id}")
+  public CarDto getCarById(@PathVariable Long id) throws CarNotFoundException {
+    return CarDto.fromEntity(carService.getById(id));
+  }
 
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public CarDto createCar(@RequestBody CarCreationDto car) {
+    return CarDto.fromEntity(carService.createCar(car.toEntity()));
+  }
 }
