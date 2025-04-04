32dff759478c777d1e16462dcbffe639b6a3eb34
Rafael Soares <r.soares2015@outlook.com>
2025-04-04 19:50:28 -0300

    feat: adicao do retorno do person vinculado

--- src/main/java/com/rafaelsisoares/parking_system/controllers/dto/CarDto.java
+++ src/main/java/com/rafaelsisoares/parking_system/controllers/dto/CarDto.java
@@ -2,8 +2,10 @@ package com.rafaelsisoares.parking_system.controllers.dto;
 
 import com.rafaelsisoares.parking_system.entities.Car;
 
-public record CarDto(Long id, String brand, String model, String plate) {
+public record CarDto(Long id, String brand, String model, String plate, PersonDto person) {
   public static CarDto fromEntity(Car car) {
-    return new CarDto(car.getId(), car.getBrand(), car.getModel(), car.getPlate());
+    PersonDto personDto = car.getPerson() != null ? PersonDto.fromEntity(car.getPerson()) : null;
+
+    return new CarDto(car.getId(), car.getBrand(), car.getModel(), car.getPlate(), personDto);
   }
 }
