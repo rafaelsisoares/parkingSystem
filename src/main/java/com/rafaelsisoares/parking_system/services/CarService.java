407e8d5a5de13b5fb38e0092cd4a2f861a1c2a8f
Rafael Soares <r.soares2015@outlook.com>
2025-04-04 19:49:58 -0300

    feat: criado metodo para vincular um person a um car

--- src/main/java/com/rafaelsisoares/parking_system/services/CarService.java
+++ src/main/java/com/rafaelsisoares/parking_system/services/CarService.java
@@ -4,16 +4,20 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.rafaelsisoares.parking_system.entities.Car;
+import com.rafaelsisoares.parking_system.entities.Person;
 import com.rafaelsisoares.parking_system.repositories.CarRepository;
 import com.rafaelsisoares.parking_system.services.exceptions.CarNotFoundException;
+import com.rafaelsisoares.parking_system.services.exceptions.PersonNotFoundException;
 
 @Service
 public class CarService {
   private CarRepository carRepository;
+  private PersonService personService;
 
   @Autowired
-  public CarService(CarRepository carRepository) {
+  public CarService(CarRepository carRepository, PersonService personService) {
     this.carRepository = carRepository;
+    this.personService = personService;
   }
 
   public List<Car> getAll() {
@@ -44,5 +48,15 @@ public class CarService {
     carRepository.delete(car);
 
     return car;
+  }
+
+  public Car setPerson(Long carId, Long personId)
+      throws CarNotFoundException, PersonNotFoundException {
+    Car car = getById(carId);
+    Person person = personService.getById(personId);
+
+    car.setPerson(person);
+
+    return carRepository.save(car);
   }
 }
