f114449f0b0082b744b6451c71a0e93c8d0ea79a
Rafael Soares <r.soares2015@outlook.com>
2025-04-04 20:14:55 -0300

    feat: criado excecao de conflito de dados

---
+++ src/main/java/com/rafaelsisoares/parking_system/services/exceptions/ConflictException.java
@@ -0,0 +1,9 @@
+package com.rafaelsisoares.parking_system.services.exceptions;
+
+public class ConflictException extends Exception {
+
+  public ConflictException(String message) {
+    super(message);
+  }
+
+}
