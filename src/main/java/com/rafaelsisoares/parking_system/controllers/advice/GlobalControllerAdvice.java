a354d6dea78d125885c6fba4f03ddf7131781cc5
Rafael Soares <r.soares2015@outlook.com>
2025-04-07 20:01:09 -0300

    feat: implementado o handler para tratar a excecao de conflito

--- src/main/java/com/rafaelsisoares/parking_system/controllers/advice/GlobalControllerAdvice.java
+++ src/main/java/com/rafaelsisoares/parking_system/controllers/advice/GlobalControllerAdvice.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import com.rafaelsisoares.parking_system.services.exceptions.ConflictException;
 import com.rafaelsisoares.parking_system.services.exceptions.NotFoundException;
 
 // Classe que centraliza o tratamento de excecoes.
@@ -12,6 +13,11 @@ public class GlobalControllerAdvice {
   @ExceptionHandler
   public ResponseEntity<String> handleNotFound(NotFoundException exception) {
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exception.getMessage());
+  }
+
+  @ExceptionHandler
+  public ResponseEntity<String> handleConflict(ConflictException exception) {
+    return ResponseEntity.status(HttpStatus.CONFLICT).body(exception.getMessage());
   }
 
   @ExceptionHandler
