c15d17fe2e162b1c199041b3d55b08a32092c67c
Rafael Soares <r.soares2015@outlook.com>
2025-04-28 19:23:56 -0300

    feat: Implementado entidade e repositorio da tabela de controle de acesso

---
+++ src/main/java/com/rafaelsisoares/parking_system/repositories/ControlAccess.java
@@ -0,0 +1,6 @@
+package com.rafaelsisoares.parking_system.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ControlAccess extends JpaRepository<ControlAccess, Long> {
+}
