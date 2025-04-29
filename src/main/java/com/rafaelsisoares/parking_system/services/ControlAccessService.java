fe8a9b24fb8c8aee98145d6c5ca2180eccc07870
Rafael Soares <r.soares2015@outlook.com>
2025-04-29 20:54:49 -0300

    chore: Implementacao do metodo de registro da entrada ou saida no report

--- src/main/java/com/rafaelsisoares/parking_system/services/ControlAccessService.java
+++ src/main/java/com/rafaelsisoares/parking_system/services/ControlAccessService.java
@@ -2,23 +2,28 @@ package com.rafaelsisoares.parking_system.services;
 
 import com.rafaelsisoares.parking_system.entities.Car;
 import com.rafaelsisoares.parking_system.entities.ControlAccess;
+import com.rafaelsisoares.parking_system.entities.Report;
 import com.rafaelsisoares.parking_system.repositories.ControlAccessRepository;
 import com.rafaelsisoares.parking_system.services.exceptions.DoubleAccessException;
 import com.rafaelsisoares.parking_system.services.exceptions.UnauthorizedPlateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Service
 public class ControlAccessService {
     private final ControlAccessRepository controlAccessRepository;
     private final CarService carService;
+    private final ReportService reportService;
 
     @Autowired
-    public ControlAccessService(ControlAccessRepository controlAccessRepository, CarService carService) {
+    public ControlAccessService(ControlAccessRepository controlAccessRepository, CarService carService, ReportService reportService) {
         this.controlAccessRepository = controlAccessRepository;
         this.carService = carService;
+        this.reportService = reportService;
     }
 
     private ControlAccess create(ControlAccess newAccess) {
@@ -33,6 +38,18 @@ public class ControlAccessService {
         return controlAccessRepository.findByPlate(plate);
     }
 
+    private void setReport(ControlAccess newAccess) {
+        DateTimeFormatter brazilianPattern = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+        Report report = new Report();
+        report.setName(newAccess.getName());
+        report.setPlate(newAccess.getPlate());
+        report.setPosition(newAccess.getPosition() ? "IN" : "OUT");
+        report.setDate(LocalDateTime.parse(LocalDateTime.now().format(brazilianPattern)));
+
+        reportService.create(report);
+    }
+
     private ControlAccess update(String plate, Boolean actualPosition) throws DoubleAccessException, UnauthorizedPlateException {
         ControlAccess result = findByPlate(plate);
         if(result == null) {
@@ -44,6 +61,7 @@ public class ControlAccessService {
             newAccess.setPlate(car.getPlate());
             newAccess.setName(car.getPerson().getName());
             newAccess.setPosition(actualPosition);
+            setReport(newAccess);
             return create(newAccess);
         }
 
@@ -51,6 +69,7 @@ public class ControlAccessService {
             throw new DoubleAccessException();
         }
         result.setPosition(actualPosition);
+        setReport(result);
         return create(result);
     }
 
